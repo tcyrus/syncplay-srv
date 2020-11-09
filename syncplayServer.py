3e660a1a762ad772c9ad50b900c20f100877a27e
Timothy Cyrus <4201229+tcyrus@users.noreply.github.com>
2020-11-08 21:01:47 -0500

    Update syncplayServer.py

--- syncplayServer.py
+++ syncplayServer.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 # coding:utf8
 
+import asyncio
 import logging
+
+# import uvloop
 
 from syncplay import ep_server
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    ep_server.main()
+    # uvloop.install()
+    asyncio.run(ep_server.main())
