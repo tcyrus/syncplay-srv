fbc42a1a37102198072e5647bca2ab5b092714ad
Timothy Cyrus <tcyrus@tcyr.us>
2021-05-09 17:44:20 -0400

    Migrate to cert-manager

--- extras/k8s-deploy.sh
+++ extras/k8s-deploy.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# kubectl create secret tls syncplay-tls-secret \
-#   "--cert=${SYNCPLAY_TLS_PATH}/fullchain.pem" \
-#   "--key=${SYNCPLAY_TLS_PATH}/privkey.pem" \
-#   --dry-run -o yaml | kubectl apply -f -
-# kubectl label secret syncplay-tls-secret app=syncplay
+kubectl label secret syncplay-tls-secret "app.kubernetes.io/part-of=syncplay"
 
 kubectl rollout restart deployment/syncplay-main
